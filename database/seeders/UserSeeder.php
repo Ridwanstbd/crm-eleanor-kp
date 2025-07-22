43b13cde0ae01bf1c2b3e3cce893da9996a2ff73
ridwanstbd <ridwansetiobudi77@gmail.com>
2025-07-22 13:35:24 +0700

    seeder perbaikan

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,6 @@ class UserSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-            'role' => 'admin',
         ]);
     }
 }
