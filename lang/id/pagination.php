30ce3e01d1b4b94ce7f2a275d7b5d6c7fc855b19
ridwanstbd <ridwansetiobudi77@gmail.com>
2025-08-14 10:40:26 +0700

    ubah bahasa inggris ke indonesia

---
+++ lang/id/pagination.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris-Baris Bahasa Paginasi
+    |--------------------------------------------------------------------------
+    |
+    | Baris-baris bahasa berikut digunakan oleh pustaka paginator untuk
+    | membuat tautan paginasi sederhana. Anda bebas mengubahnya menjadi apa pun
+    | yang Anda inginkan untuk menyesuaikan tampilan agar lebih cocok dengan
+    | aplikasi Anda.
+    |
+    */
+
+    'previous' => '&laquo; Sebelumnya',
+    'next'     => 'Berikutnya &raquo;',
+
+];
---
+++ lang/id/passwords.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris-Baris Bahasa Atur Ulang Kata Sandi
+    |--------------------------------------------------------------------------
+    |
+    | Baris-baris bahasa berikut adalah baris default yang sesuai dengan alasan
+    | yang diberikan oleh broker kata sandi untuk hasil percobaan pembaruan
+    | kata sandi, seperti kegagalan karena kata sandi / token atur ulang
+    | yang tidak valid.
+    |
+    */
+
+    'reset'     => 'Kata sandi Anda sudah diatur ulang!',
+    'sent'      => 'Kami telah mengirim email tautan atur ulang kata sandi Anda.',
+    'throttled' => 'Harap tunggu sebelum mencoba lagi.',
+    'token'     => 'Token atur ulang kata sandi ini tidak valid.',
+    'user'      => 'Kami tidak dapat menemukan pengguna dengan alamat email tersebut.',
+
+];
