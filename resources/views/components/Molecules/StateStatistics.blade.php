ba90a31b53cd9362d11d3ea7c5377007d9c317f7
ridwanstbd <ridwansetiobudi77@gmail.com>
2025-09-03 11:15:19 +0700

    refaktor 3

--- resources/views/components/Molecules/StateStatistics.blade.php
+++ resources/views/components/Molecules/StateStatistics.blade.php
@@ -2,8 +2,17 @@
     'stateStats',
     'getPercentage'
 ])
-
+<x-Atoms.Typography.Heading level="3">Keadaan Pesan</x-Atoms.Typography.Heading>
 <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+    {{-- Terkirim --}}
+    <x-Atoms.StateCard
+        title="Terkirim (WhatsApp off)"
+        :value="number_format($stateStats['Terkirim'] ?? 0)"
+        :percentage="$getPercentage($stateStats['Terkirim'] ?? 0)"
+        icon="M5 13l4 4L19 7"
+        color="gray"
+    />
+
     {{-- Tersampaikan --}}
     <x-Atoms.StateCard
         title="Tersampaikan"
@@ -20,14 +29,5 @@
         :percentage="$getPercentage($stateStats['Dibaca'] ?? 0)"
         icon="M15 12a3 3 0 11-6 0 3 3 0 016 0z M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
         color="blue"
-    />
-
-    {{-- Terkirim --}}
-    <x-Atoms.StateCard
-        title="Terkirim"
-        :value="number_format($stateStats['Terkirim'] ?? 0)"
-        :percentage="$getPercentage($stateStats['Terkirim'] ?? 0)"
-        icon="M5 13l4 4L19 7"
-        color="gray"
     />
 </div>
