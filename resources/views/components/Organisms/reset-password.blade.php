139690cde68c8de966233304b4036f9d41b3ce2b
ridwanstbd <ridwansetiobudi77@gmail.com>
2025-09-03 22:50:13 +0700

    atur reset password

--- resources/views/components/Organisms/reset-password.blade.php
+++ resources/views/components/Organisms/reset-password.blade.php
@@ -4,17 +4,17 @@
             @csrf
             <!-- Password Reset Token -->
             <input type="hidden" name="token" value="{{ $request->route('token') }}">
-            <x-Molecules.Form.FormGroup :message="$errors->get('email')" label="__('Email')" for="email">
-                <x-Atoms.Form.Input id="email" type="email" name="email" value="old('email', $request->email)" required autofocus autocomplete="username" />
+            <x-Molecules.Form.FormGroup :message="$errors->get('email')" label="Email" for="email">
+                <x-Atoms.Form.Input id="email" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
             </x-Molecules.Form.FormGroup>
-            <x-Molecules.Form.FormGroup :message="$errors->get('password')" label="__('Password')" for="password">
+            <x-Molecules.Form.FormGroup :message="$errors->get('password')" label="Kata Sandi" for="password">
                 <x-Atoms.Form.Input id="password" type="password" name="password" required autocomplete="new-password" />
             </x-Molecules.Form.FormGroup>
-            <x-Molecules.Form.FormGroup :message="$errors->get('password_confirmation')" label="__('Confirm Password')" for="password_confirmation">
+            <x-Molecules.Form.FormGroup :message="$errors->get('password_confirmation')" label="Konfirmasi Kata Sandi" for="password_confirmation">
                 <x-Atoms.Form.Input id="password_confirmation" type="password" name="password_confirmation" required autocomplete="new-password" />
             </x-Molecules.Form.FormGroup>
             <x-Atoms.Form.ButtonSubmit>
-                {{ __('Reset Password') }}
+                Atur Ulang Kata Sandi
             </x-Atoms.Form.ButtonSubmit> 
         </form>
     </x-Layouts.FormAuthContainer>
